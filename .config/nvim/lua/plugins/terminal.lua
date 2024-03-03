79d40c6b779125f0e289ae6f520dc6aa867005de
Saquib Mian <saquib.mian@gmail.com>
2024-03-03 14:56:52 -0500

    Lazygit as a built-in terminal in NVim

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -3,10 +3,20 @@ return {
 		-- ToggleTerm opens a terminal in a floating window.
 		"akinsho/toggleterm.nvim",
 		version = "*",
-		opts = {
-			open_mapping = "\\t",
-			direction = "float",
-		},
-		config = true,
+		config = function()
+			require("toggleterm").setup({
+				open_mapping = "\\t",
+				direction = "float",
+			})
+			-- lazygit terminal bound to \-g
+			local Terminal = require("toggleterm.terminal").Terminal
+			local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+			local function _lazygit_toggle()
+				lazygit:toggle()
+			end
+			vim.keymap.set("n", "\\g", function()
+				_lazygit_toggle()
+			end, { noremap = true, silent = true })
+		end,
 	},
 }

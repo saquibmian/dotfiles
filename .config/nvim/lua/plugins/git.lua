4db87f2600cf5ee23e80910ebc49d6646f12a565
Saquib Mian <saquib.mian@gmail.com>
2024-05-27 11:31:10 -0400

    Nav git hunks with [c ]c

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -14,6 +14,33 @@ return {
 	{
 		-- Gitsigns shows Git change markers in statusline.
 		"lewis6991/gitsigns.nvim",
-		config = true,
+		config = {
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.next_hunk()
+					end
+				end)
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.prev_hunk()
+					end
+				end)
+			end,
+		},
 	},
 }

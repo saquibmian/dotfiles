362bdb5fa6d7628e397242317ba659d7344dc87c
Saquib Mian <saquib.mian@gmail.com>
2024-05-27 11:24:45 -0400

    Show mode false

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,18 @@
 -- These are my personal VIM options.
 vim.g.mapleader = " " -- Override the leader from <\> to <Space>. This can cause some issues, but so far I like it.
+vim.g.maplocalleader = " "
+
+vim.opt.showmode = false
+
 vim.opt.nu = true -- Enable line numbers
 vim.opt.relativenumber = true -- Enable relative line numbers.
+
 -- All 4 of these options are set together. For your sanity, the first three should match.
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+
 -- Highlight searches as I'm typing, and preview replacements.
 vim.opt.hlsearch = false
 vim.opt.incsearch = true

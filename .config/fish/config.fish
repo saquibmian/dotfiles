b8980923ac89dfb01d2eef84aaec95a6103c0c6a
Saquib Mian <saquib.mian@gmail.com>
2024-10-02 13:01:33 -0400

    set XDG_CONFIG_HOME

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ fish_add_path ~/go/bin
 fish_add_path ~/.bun/bin
 
 set -x FZF_DEFAULT_COMMAND 'rg --files'
+set -x XDG_CONFIG_HOME "$HOME/.config"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here

8b25882de757296075991a653ee2a8825511a188
Saquib Mian <saquib.mian@gmail.com>
2024-11-29 12:00:20 -0500

    go codelenses config

--- .config/nvim/lua/plugins/lsp/settings/gopls.lua
+++ .config/nvim/lua/plugins/lsp/settings/gopls.lua
@@ -11,6 +11,13 @@ return {
 		rangeVariableTypes = true,
 	},
 	codelenses = {
+		gc_details = false,
+		generate = true,
+		regenerate_cgo = true,
+		run_govulncheck = false,
 		test = true,
+		tidy = true,
+		upgrade_dependency = true,
+		vendor = true,
 	},
 }

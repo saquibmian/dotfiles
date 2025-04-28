74aeaf22db6f1c913daddb3341a3e21306378929
Saquib Mian <saquib.mian@gmail.com>
2025-04-28 12:02:38 -0400

    Update lazy deps

--- .config/nvim/lua/plugins/lsp/settings/gopls.lua
+++ .config/nvim/lua/plugins/lsp/settings/gopls.lua
@@ -1,15 +1,6 @@
 return {
 	semanticTokens = true,
 	linksInHover = false,
-	hints = {
-		assignVariableTypes = true,
-		compositeLiteralFields = true,
-		compositeLiteralTypes = true,
-		constantValues = true,
-		functionTypeParameters = true,
-		parameterNames = true,
-		rangeVariableTypes = true,
-	},
 	codelenses = {
 		gc_details = false,
 		generate = true,

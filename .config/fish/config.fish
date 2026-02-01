5cf8098a357b03ba2293a2558d5c142d05d819f1
Saquib Mian <saquib.mian@gmail.com>
2026-01-31 21:08:06 -0500

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ set -gx EDITOR nvim
 fish_add_path ~/.cargo/bin
 fish_add_path ~/go/bin
 fish_add_path ~/.bun/bin
+fish_add_path ~/.local/bin
 
 set -x FZF_DEFAULT_COMMAND 'rg --files'
 set -x XDG_CONFIG_HOME "$HOME/.config"
@@ -15,6 +16,3 @@ end
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-
-# Created by `pipx` on 2025-04-15 16:52:41
-fish_add_path ~/.local/bin

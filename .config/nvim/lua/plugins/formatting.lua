c9f634ad98064054f40fd3fa1fb399f3414877f5
Saquib Mian <saquib.mian@gmail.com>
2026-02-14 11:15:28 -0500

    format with prettier

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -14,6 +14,8 @@ return {
 				lua = { "stylua" },
 				python = { "isort", "black" },
 				javascript = { "prettierd", "prettier" },
+				css = { "prettierd", "prettier" },
+				html = { "prettierd", "prettier" },
 				json = { "jq" },
 				sql = { "pg_format" },
 			},
